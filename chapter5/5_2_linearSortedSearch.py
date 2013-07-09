084a5a16650a827a99ef951414b371a078a6f66c
shenzhun <shenzhunallen@gmail.com>
2013-07-09 21:56:37 +0800

    5.1 listing for unsorted linear search

--- chapter5/5_2_linearSortedSearch.py
+++
@@ -1,9 +0,0 @@
-def sorteLinearSearch( theValue, item):
-    n = len( theValue )
-    for i in range( n ):
-        if theValue[i] == item :
-            return True
-        elif theValue[i] > item :
-            return False
-    return False
-
