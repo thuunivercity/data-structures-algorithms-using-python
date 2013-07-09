c2486807db6eddf39745f870f45a5e20c021bad2
shenzhun <shenzhunallen@gmail.com>
2013-07-10 05:11:43 +0800

    rm uncorrect bubble sorting

--- chapter5/bubbleSearch.py
+++
@@ -1,9 +0,0 @@
-def bubbleSort(theSeq):
-    n = len(theSeq)
-    for i in range(n-1):
-        for j in range(n-1):
-            if theSeq[j] > theSeq[j+1]:
-                tmp = theSeq[j]
-                theSeq[j] = theSeq[j+1]
-                theSeq[j+1] = tmp
-
