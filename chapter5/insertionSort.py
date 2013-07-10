3a14ac6c51f9981eb87a4c27ff8d7faa5b7af8c2
shenzhun <shenzhunallen@gmail.com>
2013-07-10 18:28:33 +0800

    5.7 listing for inserttion sort, add comments and testing

--- chapter5/insertionSort.py
+++ chapter5/insertionSort.py
@@ -1,16 +1,23 @@
 def insertionSort(theSeq):
-    n = len(theSeq)
+    n = len(theSeq)i
+     # Starts with the first item as the only sorted entry.
     for i in range(1, n):
+         # Save the value to be positioned.
         value = theSeq[i]
+         # Find the position where value fits in the orderd part of the list.
         pos = i - 1
         while pos >= 0 and value < theSeq[pos]:
+             # Shift the items to the right during the search
             theSeq[pos+1] = theSeq[pos]
             pos = pos - 1
+         # Put the saved value in to the open slot.
         theSeq[pos+1] = value
+        print('iteration#{}: {}'.format(i, theSeq))
+
     return theSeq
 
 #test the insertion sorting method using instance
 testlist = [27, 33, 28, 4, 2, 26, 13, 35, 8, 14]
-print testlist
-print insertionSort(testlist)
+print('Before insertion sort:'.format(testlist))
+print('After insertion sort:'.format(insertionSort(testlist)))
     
