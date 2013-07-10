6e3007e9619e8078fd19297bd0c53b7a4c7e3da2
shenzhun <shenzhunallen@gmail.com>
2013-07-10 18:59:42 +0800

    5.5 listing for bubble sort, add comments and testing

--- chapter5/bubbleSort.py
+++ chapter5/bubbleSort.py
@@ -1,14 +1,20 @@
 #The author made a mistake, and I modify it. Pls check it carfully.
 
-def bubbleSort(list):
-    n = len(list)
+# Sorted a sequence in ascending order using the buble sort algoithm.
+def bubbleSort(theSeq):
+    n = len(theSeq)
+     # Perform n-1 bubble operations on the sequence
     for i in range(n-1):
+         # Bubble the largest item to the end
         for j in range(n-1-i):
-            if list[j] > list[j+1]:
-                # this formula is more pythonic.
-                list[j], list[j+1] = list[j+1], list[j]
-    return list
+            if theSeq[j] > theSeq[j+1]:
+                 # Swap the j and j+1 items
+                theSeq[j], theSeq[j+1] = theSeq[j+1], theSeq[j]
+            print('iteration #{}: {}'.format(i, theSeq))
+
+    return theSeq
+#test bubble sort using test instance
 
 testlist = [27, 33, 28, 4, 2, 26, 13, 35, 8, 14]
-print testlist
-print bubbleSort(testlist)
+print('Before bubble sort:{}'.format(testlist))
+print('After bubble sort:{}'.format(bubbleSort(testlist)))
